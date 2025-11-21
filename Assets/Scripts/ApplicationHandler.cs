b214bffd8505e8e2bb639ab216484289a834605e
linn006 <c25linha@student.his.se>
2025-11-21 13:32:09 +0100

    La till assets och fixade en startskärm + la till scenen som spelet ska befinna sig i

--- Assets/Scripts/ApplicationHandler.cs
+++ Assets/Scripts/ApplicationHandler.cs
@@ -10,7 +10,7 @@ public class ApplicationHandler : MonoBehaviour
     public void ChangeScene()
     {
         // Load the scene named "NewScene"
-        SceneManager.LoadSceneAsync("NewScene", LoadSceneMode.Single);
+        SceneManager.LoadSceneAsync("GameScene", LoadSceneMode.Single);
     }
 
     public void QuitGame()
